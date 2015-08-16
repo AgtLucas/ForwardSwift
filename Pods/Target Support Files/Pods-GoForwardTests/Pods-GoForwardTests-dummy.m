3b9bee3a4e7802967057ed40dfc60ac473b264f6
Lucas <lucas@agtlucas.com>
2015-08-16 09:56:09 -0300

    Adding Pod

---
+++ Pods/Target Support Files/Pods-GoForwardTests/Pods-GoForwardTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GoForwardTests : NSObject
+@end
+@implementation PodsDummy_Pods_GoForwardTests
+@end
---
+++ Pods/Target Support Files/Pods-GoForwardUITests/Pods-GoForwardUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_GoForwardUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GoForwardUITestsVersionString[];
+
